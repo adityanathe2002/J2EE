fd8a67fe1d2f3d9486466c1dda4d088fd61678df
Aditya <adityanathe72@gmail.com>
2024-09-02 11:38:49 +0530

    some update

--- jdbc/registration_app/src/main/java/com/qsp/registration_app/controller/StudentFetchServlet.java
+++ jdbc/registration_app/src/main/java/com/qsp/registration_app/controller/StudentFetchServlet.java
@@ -1,35 +1,35 @@
-package com.qsp.registration_app.controller;
-
-import java.io.IOException;
+//package com.qsp.registration_app.controller;
+//
 //import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.qsp.reg_app.dao.StudentDao;
-import com.qsp.reg_app.dto.Student;
-import com.qsp.registration_app.dao.studentDao;
-import com.qsp.registration_app.dto.student;
-
-@WebServlet(value = "/fetchstudent")
-public class StudentFetchServlet extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+////import java.io.IOException;
+//import java.util.List;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import com.qsp.reg_app.dao.StudentDao;
+//import com.qsp.reg_app.dto.Student;
+//import com.qsp.registration_app.dao.studentDao;
+//import com.qsp.registration_app.dto.student;
+//
+//@WebServlet(value = "/fetchstudent")
+//public class StudentFetchServlet extends HttpServlet {
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 //		List<student> students = studentDao.fetchByAll();
 //		req.setAttribute("students", students);
 //		RequestDispatcher dispatcher = req.getRequestDispatcher("studentList.jsp");
 //		dispatcher.forward(req, resp);
-
-		studentDao dao=new studentDao();
-		List<student> students =dao.fetchByAll();
-		req.setAttribute("list", students);
-		RequestDispatcher rd=req.getRequestDispatcher("studentlist.jsp");
-		rd.forward(req, resp);
-	}
-}
+//
+//		studentDao dao=new studentDao();
+//		List<student> students =dao.fetchByAll();
+//		req.setAttribute("list", students);
+//		RequestDispatcher rd=req.getRequestDispatcher("studentlist.jsp");
+//		rd.forward(req, resp);
+//	}
+//}
